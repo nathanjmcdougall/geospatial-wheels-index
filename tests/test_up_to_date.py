d827841e3cf32f1f7ede145866ec86cace842142
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-02-28 12:52:01 +1300

    Use absolute paths

--- tests/test_up_to_date.py
+++ tests/test_up_to_date.py
@@ -58,7 +58,7 @@ def test_index_up_to_date(tmp_path: Path) -> None:
 """
         + "\n".join(
             [
-                f"""    <a href="geospatial-wheels-windows-flatlinks/{whl_name}/">{whl_name}</a>"""
+                f"""    <a href="https://nathanjmcdougall.github.io/geospatial-wheels-windows-flatlinks/{whl_name}/">{whl_name}</a>"""
                 for whl_name in package_names
             ]
         )
